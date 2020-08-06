639aa1c25f91c0ec7c10d20c5f6fa2412b38182e
Yann de The <ydethe@gmail.com>
2020-08-06 13:47:46 +0000

    black

--- AlgebraicNumber/utils.py
+++ AlgebraicNumber/utils.py
@@ -10,10 +10,10 @@ import scipy.signal as sig
 import scipy.linalg as lin
 
 
-def nCr(n, r):
-    return factorial(n) // factorial(r) // factorial(n - r)
-
-
+def nCr(n,r):
+    return factorial(n) // factorial(r) // factorial(n-r)
+    
+    
 def nint(x: np.complex64) -> np.int64:
     """
     
@@ -325,31 +325,31 @@ def kron_add(a: np.array, b: np.array) -> np.array:
     return Q
 
 
-def sylvester(R, S):
-    """
+def sylvester(R,S):
+    '''
     
     Examples:
       >>> R = [1, -3, 1, -3, 1]
       >>> S = [-3, 2, -9, 4]
       >>> m = sylvester(R, S)
       
-    """
+    '''
     r = P.polytrim(R)
     s = P.polytrim(S)
-
-    m = len(r) - 1
-    n = len(s) - 1
-    res = np.zeros((n + m, n + m))
+    
+    m = len(r)-1
+    n = len(s)-1
+    res = np.zeros((n+m,n+m))
     for k in range(n):
-        res[k, k : k + m + 1] = r[-1::-1]
+        res[k,k:k+m+1] = r[-1::-1]
     for k in range(m):
-        res[k + n, k : k + n + 1] = s[-1::-1]
-
+        res[k+n,k:k+n+1] = s[-1::-1]
+    
     return res
-
-
+    
+    
 def resultant(R, S):
-    """
+    '''
     
     Examples:
       >>> R = [1, -3, 1, -3, 1]
@@ -357,13 +357,13 @@ def resultant(R, S):
       >>> resultant(R, S)
       -4563.0
       
-    """
-    m = sylvester(R, S)
+    '''
+    m = sylvester(R,S)
     return lin.det(m)
-
-
+    
+    
 def discriminant(R):
-    """
+    '''
     
     Examples:
       >>> R = [1, -3, 1, -3]
@@ -372,25 +372,25 @@ def discriminant(R):
       >>> np.abs(d-d0) < 1e-10
       True
       
-    """
+    '''
     r = P.polytrim(R)
-    n = len(r) - 1
-
-    if ((n * (n - 1)) // 2) % 2 == 0:
+    n = len(r)-1
+    
+    if ((n*(n-1))//2) % 2 == 0:
         s = 1
     else:
         s = -1
-
+        
     dp = P.polyder(r)
     res = resultant(r, dp)
-
-    dis = s * res / r[-1]
-
+    
+    dis = s*res/r[-1]
+    
     return dis
-
-
+    
+    
 def mahler_separation_bound(R: np.array) -> float:
-    r"""The minimum root separation for a polynomial P is defined as:
+    r'''The minimum root separation for a polynomial P is defined as:
     
     .. math::
         sep(P) = min(|r-s|, (r,s) \in roots(P), r \neq s)
@@ -409,18 +409,18 @@ def mahler_separation_bound(R: np.array) -> float:
       >>> b
       0.111...
       
-    """
+    '''
     r = P.polytrim(R)
-    n = len(r) - 1
-
+    n = len(r)-1
+    
     d = discriminant(R)
-    sep = np.sqrt(3 * np.abs(d) / n ** (n + 2)) / lin.norm(r, ord=2) ** (n - 1)
-
+    sep = np.sqrt(3*np.abs(d)/n**(n+2))/lin.norm(r,ord=2)**(n-1)
+    
     return sep
-
-
+    
+    
 def polygcd(R, S):
-    r"""
+    r'''
     
     Examples:
       >>> R = [1, -1, 0, 0, -1, 1]
@@ -428,81 +428,100 @@ def polygcd(R, S):
       >>> polygcd(R, S)
       array([-1.,  1.]...
       
-    """
+    '''
     a = P.polytrim(R)
     b = P.polytrim(S)
-
-    m = len(a) - 1
-    n = len(b) - 1
-
-    if n > m:
-        a, b = b, a
-        n, m = m, n
-
+    
+    m = len(a)-1
+    n = len(b)-1
+    
+    if n>m:
+        a,b = b,a
+        n,m = m,n
+        
     # Here, deg(a) >= deb(b)
-
+    
     while True:
-        q, r = P.polydiv(a, b)
-
-        a, b = b, r
-
+        q,r = P.polydiv(a,b)
+        
+        a,b = b,r
+        
         if len(b) == 1 and b[0] == 0:
-            return a / a[-1]
-
-
+            return a/a[-1]
+    
+    
 def square_free_fact(R):
-    r"""
+    r'''
     
     Examples:
       >>> R = [1, -1, 0, 0, -1, 1]
       >>> square_free_fact(R)
       array([-1,  0,  0,  0,  1]...
       
-    """
+    '''
     p = P.polytrim(R)
-    n = len(p) - 1
-
+    n = len(p)-1
+    
     while True:
         dp = P.polyder(p)
         g = polygcd(p, dp)
         if len(g) == 1 and g[0] == 1:
-            return np.int32(np.round(p, 0))
-
-        p, r = P.polydiv(p, g)
+            return np.int32(np.round(p,0))
+        
+        p,r = P.polydiv(p,g)
         if len(r) != 1 or r[0] != 0:
             raise AssertionError(r)
-
-
+        
+    
 def polytrans(R, a):
-    r"""
+    r'''
     
     Examples:
       >>> R = [0, 0, 1]
       >>> polytrans(R, 2)
       array([4., 4., 1.]...
       
-    """
+    '''
     r = P.polytrim(R)
-    n = len(r) - 1
-
-    q = np.empty(n + 1)
-    for k in range(n + 1):
+    n = len(r)-1
+    
+    q = np.empty(n+1)
+    for k in range(n+1):
         q[k] = 0
-        for p in range(k, n + 1):
-            q[k] += nCr(p, k) * r[p] * a ** (p - k)
-
+        for p in range(k,n+1):
+            q[k] += nCr(p,k)*r[p]*a**(p-k)
+    
     return q
-
-
+    
+    
 def npolymul(*polynomials):
     res = [1]
     for q in polynomials:
         res = P.polymul(res, q)
     return res
-
-
+    
+    
+def is_poly_valid(h, tol):
+    '''The coeficients of the polynomial h must:
+    * have no imaginary part
+    * be integers
+    '''
+    ok = 0
+    for hk in h:
+        if np.abs(np.imag(hk)) > tol:
+            ok = 1
+            break
+            
+        d,_ = modf(np.real(hk))
+        if np.abs(d) > tol:
+            ok = 2
+            break
+    
+    return ok
+    
+    
 def simplify(h, root, tol=1e-9):
-    r"""
+    r'''
     
     Examples:
       >>> R = [4, -2, 0, -2, 1]
@@ -511,71 +530,55 @@ def simplify(h, root, tol=1e-9):
       >>> simplify(R, 2**(1/3))
       array([-2,  0,  0,  1]...
       
-    """
+    '''
     # 1. square free
     h2 = square_free_fact(h)
-
-    g = reduce(gcd, h2)
-    h3 = h2 // g
-
-    n = len(h3) - 1
+    
+    g = reduce(gcd,h2)
+    h3 = h2//g
+    
+    n = len(h3)-1
     roots = P.polyroots(h3)
-
+    
     # We remove the given root
-    yy = np.abs(roots - root)
+    yy = np.abs(roots-root)
     i0 = np.argmin(yy)
     roots = np.delete(roots, i0)
-
-    def _is_poly_valid(h):
-        """The coeficients of the polynomial h must:
-        * have no imaginary part
-        * be integers
-        """
-        ok = True
-        for hk in h:
-            if np.abs(np.imag(hk)) > tol:
-                ok = False
-                break
-
-            d, _ = modf(np.real(hk))
-            if np.abs(d) > tol:
-                ok = False
-                break
-
-        return ok
-
-    if n <= 2:
+    
+    if n <= 1:
         return h3
-
+    
     for nr in range(n):
         for c in combinations(roots, nr):
-            h4 = npolymul(*[[-x, 1] for x in c])
-            h4 = P.polymul(h4, [-root * h3[-1], h3[-1]])
-
-            ok = _is_poly_valid(h4)
-
-            if ok:
+            h4 = npolymul(*[[-x,1] for x in c])
+            h4 = P.polymul(h4, [-root*h3[-1],h3[-1]])
+            
+            ok = is_poly_valid(h4, tol)
+            
+            if ok == 0:
                 break
-
-        if ok:
+                
+        if ok == 0:
             break
-
-    if not ok:
-        raise AssertionError(h3, h4)
-
+    
+    if ok != 0:
+        raise AssertionError(ok,h3,h4)
+    
     h5 = np.int32(np.real(h4))
-
+    
     # Suppressing the first null coefficients
-    n = len(h5) - 1
-
-    for i in range(n + 1):
+    n = len(h5)-1
+    
+    for i in range(n+1):
         if h5[i] != 0:
             break
-
+    
     return h5[i:]
-
+    
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod(optionflags=doctest.ELLIPSIS)
+    
+    
